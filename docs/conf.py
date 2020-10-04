c48def30243f5bba1ffef1b51719355206ad2733
pelmini <vivianbai77@gmail.com>
2020-10-04 13:59:23 -0700

    📝: Change documentation them to Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
+# pylint: disable=invalid-name
+
 import os
 import sys
+import sphinx_rtd_theme  # pylint: disable=unused-import
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -31,7 +35,9 @@ release = "0.1.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc", "sphinx.ext.napoleon"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +53,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
+html_show_sphinx = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
