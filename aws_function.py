44e064c02e6e9eea06cb1e127b43c0eed49e4a5a
pelmini <pelmini@pelminis-MBP.attlocal.net>
2021-02-05 20:45:56 -0800

    📝: Add docstrings

--- aws_function.py
+++ aws_function.py
@@ -1,9 +1,20 @@
+"""
+Python file for AWS Lambda function handler in order to help process events.
+"""
 import json
+from typing import Dict, ContextManager
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict, context: ContextManager) -> Dict:
+    """Function that when invoked, Lambda run the handler method
+
+    Args:
+        event (Dict): JSON formatted doc that contains data for lambda function to process
+        context (ContextManager): Provides methods and properties that provide information
+        about the invocation, function, and execution environment
+
+    Returns:
+        JSON payload that contains status code and message.
+    """
     print("Hello from Lambda!")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
