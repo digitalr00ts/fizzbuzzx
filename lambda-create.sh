1fb3820878d4dd134daf3bbadc407a24fe2d880e
pelmini <pelmini@pelminis-MBP.attlocal.net>
2021-03-15 13:52:06 -0700

    📝: Add comment that includes why adding zip file in lambda-create.sh as well as commands to generate zip file

--- lambda-create.sh
+++ lambda-create.sh
@@ -5,6 +5,12 @@ aws lambda create-function \
     --runtime "python3.8" \
     --role "arn:aws:iam::019263404189:role/fizzbuzzx_role" \
     --handler "aws_function.lambda_handler" \
+    # To deploy the function code to lambda, a deployment package is needed.
+    # Lambda supports two types of packages: container images and .zip files. 
+    # AWS documentation on deploying python lambda functions with .zip: 
+    # https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+    # Command to make .zip file deployment package:
+    #       zip -r <file_name>.zip <folder_to_compress>
     --zip-file "fileb://aws_fizzbuzzx.zip" \
     --timeout 5 \
     --region "us-east-2"
