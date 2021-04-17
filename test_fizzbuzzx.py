4ade72d027b543744b98f3737193b16a4005a5b0
Carlos Meza <carlos@digitalr00ts.com>
2021-04-16 23:07:39 -0700

    🎨 Fix import modules order

--- test_fizzbuzzx.py
+++ test_fizzbuzzx.py
@@ -1,5 +1,5 @@
 """Test file for fizzbuzz"""
-from fizzbuzzx import fizzbuzz, check_fizzbuzz, check_buzz, check_fizz
+from fizzbuzzx import check_buzz, check_fizz, check_fizzbuzz, fizzbuzz
 
 
 def test_check_fizz():
